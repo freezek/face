1863b14f2bb386de34758a9a67e56c41bd08389a
chentao <ikaimencxo@gmail.com>
2020-01-05 14:35:28 +0800

    init project

--- face-provider/src/main/java/com/freezk/face/controller/ProviderController.java
+++ face-provider/src/main/java/com/freezk/face/controller/ProviderController.java
@@ -1,20 +1,26 @@
 package com.freezk.face.controller;
 
 import com.alibaba.nacos.client.utils.JSONUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.w3c.dom.ls.LSOutput;
 
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.UnknownHostException;
 import java.util.Random;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/provider")
+@Slf4j
 public class ProviderController {
     @RequestMapping(value = "/echo/{source}", method = RequestMethod.GET)
-    public String echo(@PathVariable String source) {
+    public String echo(@PathVariable String source) throws UnknownHostException {
+        log.info(InetAddress.getLocalHost().getHostAddress().toString());
         return "Hello Nacos I'm server provider !" +source+"--" +new Random().nextInt(1000);
     }
 }
