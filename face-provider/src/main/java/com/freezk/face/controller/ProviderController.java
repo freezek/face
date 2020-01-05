71c9e0646b5cb03d9189a7707ae31347fd08a00b
chentao <ikaimencxo@gmail.com>
2020-01-05 14:24:10 +0800

    添加参数

--- face-provider/src/main/java/com/freezk/face/controller/ProviderController.java
+++ face-provider/src/main/java/com/freezk/face/controller/ProviderController.java
@@ -1,15 +1,20 @@
 package com.freezk.face.controller;
 
+import com.alibaba.nacos.client.utils.JSONUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Random;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/provider")
 public class ProviderController {
     @RequestMapping(value = "/echo/{source}", method = RequestMethod.GET)
     public String echo(@PathVariable String source) {
-        return "Hello Nacos I'm server provider !";
+        return "Hello Nacos I'm server provider !" +source+"--" +new Random().nextInt(1000);
     }
 }
